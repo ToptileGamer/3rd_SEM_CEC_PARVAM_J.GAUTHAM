3b354153cb4f55629d1ed8b96b1c449b6c607c99
ToptileGamer <jgautham42@gmail.com>
2025-10-15 16:04:23 +0530

    day6

---
+++ react-files/react-starter/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+const app = () => {
+
+  return (
+    <>
+    <Header />{" "}
+    <div>
+    <h1>Hello from react</h1>
+    <h2>i am gautham</h2>
+    </div>
+  </>
+  );
+}
+export default app;
---
+++ react-files/react-starter/src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <Header>
+        <h1>this is my header</h1>
+        </Header>
+    );
+}
+export default Header;
